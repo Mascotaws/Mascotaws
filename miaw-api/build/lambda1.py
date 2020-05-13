0ef9a1d5c5d07ecc67569d982c4f5fd2689373cb
SergiusxD <sbellott@hotmail.com>
2020-05-13 17:08:57 +0000

    Corrigiendo

--- miaw-api/build/lambda1.py
+++ miaw-api/build/lambda1.py
@@ -4,9 +4,10 @@ from boto3.dynamodb.conditions import Key, Attr
 
 def lambda_handler(event, context):
     dynamodb = boto3.resource('dynamodb')
-    datos= dynamodb.Table('NOMBRE DE LA TABLA')
+    #Nombre de la tabala
+    datos= dynamodb.Table('pets')
+    # datos obtenidos de la tabla
     items = datos.scan()
-
     return {
         'statusCode': 200,
         'body': items
