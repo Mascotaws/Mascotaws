fe7e69e027dfd891ddce683ecbaee378181790ea
EC2 Default User <ec2-user@ip-172-31-23-133.us-east-2.compute.internal>
2020-05-13 21:10:07 +0000

    bra3sdadasd

--- miaw-migration/src/lambdaMigration.py
+++ miaw-migration/src/lambdaMigration.py
@@ -11,10 +11,17 @@ def lambda_handler(event, context):
    for record in event['Records']:
         bucket = record['s3']['bucket']['name']
         keylave = unquote_plus(record['s3']['object']['key'])
-        if keylave.find(".csv"):
+        print()
+        if keylave.count(".csv")==1:
             object_file = s3_client.get_object(Bucket=bucket, Key=keylave)
-            initial_df = pd.read_csv(object_file['Body'], skiprows=[1,2,3])
-            print(initial_df['health'])
+            tamano=pd.read_csv(object_file['Body'])
+            print(tamano.shape[0])
+            for i in range(0,tamano.shape[0]):
+                lista=list(range(0,tamano.shape[0])) 
+                lista.remove(i)
+                object_file2 = s3_client.get_object(Bucket=bucket, Key=keylave)
+                initial_df = pd.read_csv(object_file2['Body'],skiprows=[0,2,3,4,5])
+                print(initial_df)
         else:
             copy_source = { 
                 'Bucket': bucket, 
@@ -24,8 +31,3 @@ def lambda_handler(event, context):
             tpkey=archi+keylave
             s3_client.copy_object(Bucket='upb-convert-image-lambda-lab-level1',CopySource=copy_source, Key=tpkey)
             print(bucket,keylave)
-        
-        
-        
-        
-        
