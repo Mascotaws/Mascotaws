4bb7c0fb1b6fecb3e03de96d9fa2894e904815f4
EC2 Default User <ec2-user@ip-172-31-23-133.us-east-2.compute.internal>
2020-05-13 02:27:47 +0000

    sdfdf

--- miaw-migration/src/lambdaMigration.py
+++ miaw-migration/src/lambdaMigration.py
@@ -1,5 +1,7 @@
 import boto3
 from urllib.parse import unquote_plus
+import io
+import pandas as pd
 
 
 s3_client = boto3.client('s3')
@@ -9,16 +11,21 @@ def lambda_handler(event, context):
    for record in event['Records']:
         bucket = record['s3']['bucket']['name']
         keylave = unquote_plus(record['s3']['object']['key'])
-        copy_source = { 
-            'Bucket': bucket, 
-            'Key': keylave
-        } 
-        archi='media/'
-        tpkey=archi+keylave
-        s3_client.copy_object(Bucket='upb-convert-image-lambda-lab-level1',CopySource=copy_source, Key=tpkey)
-        print(bucket,keylave)
-        
-        
+        if keylave.find(".csv"):
+            object_file = s3_client.get_object(Bucket=bucket, Key=keylave)
+            initial_df = pd.read_csv(object_file['Body'])
+            for label, content in initial_df.items():
+                print('label:', label)
+                print('content:', content, sep='\n')
+        else:
+            copy_source = { 
+                'Bucket': bucket, 
+                'Key': keylave
+            } 
+            archi='media/'
+            tpkey=archi+keylave
+            s3_client.copy_object(Bucket='upb-convert-image-lambda-lab-level1',CopySource=copy_source, Key=tpkey)
+            print(bucket,keylave)
         
         
         
