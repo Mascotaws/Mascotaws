2e58acec48dd2db1ce42e38110a8be2ccac8b378
EC2 Default User <ec2-user@ip-172-31-23-133.us-east-2.compute.internal>
2020-05-14 11:48:52 +0000

    fsfdj

--- miaw-migration/src/lambdaMigration.py
+++ miaw-migration/src/lambdaMigration.py
@@ -15,19 +15,6 @@ def lambda_handler(event, context):
    for record in event['Records']:
         bucket = record['s3']['bucket']['name']
         keylave = unquote_plus(record['s3']['object']['key'])
-<<<<<<< HEAD
-        print()
-        if keylave.count(".csv")==1:
-            object_file = s3_client.get_object(Bucket=bucket, Key=keylave)
-            tamano=pd.read_csv(object_file['Body'])
-            print(tamano.shape[0])
-            for i in range(0,tamano.shape[0]):
-                lista=list(range(0,tamano.shape[0])) 
-                lista.remove(i)
-                object_file2 = s3_client.get_object(Bucket=bucket, Key=keylave)
-                initial_df = pd.read_csv(object_file2['Body'],skiprows=[0,2,3,4,5])
-                print(initial_df)
-=======
         if keylave.count(".csv")==1:
             object_file = s3_client.get_object(Bucket=bucket, Key=keylave)
             df=pd.read_csv(object_file['Body'])
@@ -67,8 +54,6 @@ def lambda_handler(event, context):
                         Item=student
                     )
             print("Done!")
-                
->>>>>>> miaw-vivian
         else:
             copy_source = { 
                 'Bucket': bucket, 
